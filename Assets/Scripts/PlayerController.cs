6d8d0e0edb87e6947fabd0686707c8a79212a779
Skeleton <chaos0824@naver.com>
2021-01-01 22:27:57 +0900

    플레이어 입력 관리 스크립트 구현

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Behaviours")]
+    [SerializeField] private PlayerCameraBehaviour CameraBehaviour = null;
+    [SerializeField] private PlayerMovementBehaviour MovementBehaviour = null;
+
+    private Vector3 inputMovement = Vector3.zero;
+    private Vector2 inputTurn = Vector2.zero;
+
+    private void Update()
+    {
+        CameraBehaviour.UpdateRotateDirection(inputTurn);
+        MovementBehaviour.UpdateMovementDirection(inputMovement);
+    }
+
+    public void OnMovement(InputAction.CallbackContext value)
+    {
+        var movement = value.ReadValue<Vector2>();
+        inputMovement.x = movement.x;
+        inputMovement.z = movement.y;
+    }
+
+    public void OnLook(InputAction.CallbackContext value)
+    {
+        var turn = value.ReadValue<Vector2>();
+        inputTurn.x = -turn.y;
+        inputTurn.y = turn.x;
+    }
+}
