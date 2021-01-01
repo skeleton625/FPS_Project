1f9a17cfddb13a917c77fbe1f2e0386fa2fefdcc
Skeleton <chaos0824@naver.com>
2021-01-01 22:27:41 +0900

    캐릭터, 카메라의 물리적 이동 관리 스크립트 구현

---
+++ Assets/Scripts/PlayerCameraBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraBehaviour : MonoBehaviour
+{
+    [SerializeField] private float RotateSensitive = 5f;
+
+    private float preRotateSensitive = 0;
+    private Vector3 rotateDirection = Vector3.zero;
+    private Vector3 preCameraRotation = Vector3.zero;
+
+    private void Awake()
+    {
+        preCameraRotation = transform.rotation.eulerAngles;
+        preRotateSensitive = RotateSensitive * Time.deltaTime;
+    }
+
+    public void UpdateRotateDirection(Vector2 newRotateDirection)
+    {
+        rotateDirection = newRotateDirection;
+        rotateDirection.z = 0;
+    }
+
+    public void FixedUpdate()
+    {
+        TurnTheCamera();
+    }
+
+    private void TurnTheCamera()
+    {
+        rotateDirection *= preRotateSensitive;
+        preCameraRotation.x = Mathf.Clamp(preCameraRotation.x + rotateDirection.x, -90, 90);
+        preCameraRotation.y = Mathf.Repeat(preCameraRotation.y + rotateDirection.y, 360);
+        transform.rotation = Quaternion.Euler(preCameraRotation);
+    }
+}
---
+++ Assets/Scripts/PlayerMovementBehaviour.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementBehaviour : MonoBehaviour
+{
+    [SerializeField] private int MoveSpeed;
+    [SerializeField] private Rigidbody PlayerRigidbody = null;
+
+    private Camera mainCamera = null;
+    private Vector2 playerTurnDirection = Vector2.zero;
+    private Vector3 playerMoveDirection = Vector3.zero;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void FixedUpdate()
+    {
+        MoveThePlayer();
+    }
+
+    public void UpdateTurnDirection(Vector2 newTurnDriection)
+    {
+        playerTurnDirection = newTurnDriection;
+    }
+
+    public void UpdateMovementDirection(Vector3 newMoveDirection)
+    {
+        playerMoveDirection = newMoveDirection;
+    }
+
+    private void MoveThePlayer()
+    {
+        Vector3 movement = CalculateCameraDirection(playerMoveDirection) * MoveSpeed * Time.deltaTime;
+        PlayerRigidbody.MovePosition(movement + transform.position);
+    }
+
+    private void TurnThePlayer()
+    {
+
+    }
+
+    private Vector3 CalculateCameraDirection(Vector3 moveDirection)
+    {
+        var cameraForward = mainCamera.transform.forward;
+        var cameraRight = mainCamera.transform.right;
+        
+        cameraForward.y = 0;
+        cameraRight.y = 0;
+
+        return cameraForward * moveDirection.z + cameraRight * moveDirection.x;
+    }
+}
