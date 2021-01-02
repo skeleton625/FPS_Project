15decf94bef4f24bc5feeaf63a7a9dc07a42ee25
Skeleton <chaos0824@naver.com>
2021-01-02 22:22:07 +0900

    캐릭터 기준 카메라 좌우 반전, 카메라 조준 기능 구현

--- Assets/Scripts/PlayerCameraBehaviour.cs
+++ Assets/Scripts/PlayerCameraBehaviour.cs
@@ -1,19 +1,32 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCameraBehaviour : MonoBehaviour
 {
     [SerializeField] private float RotateSensitive = 5f;
+    [SerializeField] private Transform ZoomTransform = null;
+    [SerializeField] private Transform CameraTransform = null;
+    [SerializeField] private Transform NormalTransform = null;
+    [SerializeField] private Transform CameraBodyTransform = null;
 
-    private float preRotateSensitive = 0;
+    private bool isAiming = false;
+    private float aimingPosX = 0f;
+    private float preRotateSensitive = 0f;
     private Vector3 rotateDirection = Vector3.zero;
     private Vector3 preCameraRotation = Vector3.zero;
+    private Coroutine aimingZoomCoroutine = null;
+    private Coroutine aimingShiftCoroutine = null;
 
     private void Awake()
     {
+        aimingPosX = CameraBodyTransform.localPosition.x;
         preCameraRotation = transform.rotation.eulerAngles;
         preRotateSensitive = RotateSensitive * Time.deltaTime;
+    }
+
+    public void FixedUpdate()
+    {
+        TurnTheCamera();
     }
 
     public void UpdateRotateDirection(Vector2 newRotateDirection)
@@ -22,9 +35,24 @@ public class PlayerCameraBehaviour : MonoBehaviour
         rotateDirection.z = 0;
     }
 
-    public void FixedUpdate()
+    public void UpdateAimingState(bool isAiming)
     {
-        TurnTheCamera();
+        this.isAiming = isAiming;
+        if(aimingZoomCoroutine != null)
+        {
+            StopCoroutine(aimingZoomCoroutine);
+        }
+        aimingZoomCoroutine = StartCoroutine(AimingZoomCoroutine());
+    }
+
+    public void UpdateAimingPosition()
+    {
+        aimingPosX *= -1;
+        if(aimingShiftCoroutine != null)
+        {
+            StopCoroutine(aimingShiftCoroutine);
+        }
+        aimingShiftCoroutine = StartCoroutine(AimingShiftCoroutine());
     }
 
     private void TurnTheCamera()
@@ -33,5 +61,57 @@ public class PlayerCameraBehaviour : MonoBehaviour
         preCameraRotation.x = Mathf.Clamp(preCameraRotation.x + rotateDirection.x, -90, 90);
         preCameraRotation.y = Mathf.Repeat(preCameraRotation.y + rotateDirection.y, 360);
         transform.rotation = Quaternion.Euler(preCameraRotation);
+    }
+
+    private IEnumerator AimingShiftCoroutine()
+    {
+        var preCameraPos = CameraBodyTransform.localPosition;
+
+        if(aimingPosX > 0)
+        {
+            while(preCameraPos.x < aimingPosX)
+            {
+                preCameraPos.x = Mathf.Lerp(preCameraPos.x, aimingPosX, .1f);
+                CameraBodyTransform.localPosition = preCameraPos;
+                yield return null;
+            }
+        }
+        else
+        {
+            while(preCameraPos.x > aimingPosX)
+            {
+                preCameraPos.x = Mathf.Lerp(preCameraPos.x, aimingPosX, .1f);
+                CameraBodyTransform.localPosition = preCameraPos;
+                yield return null;
+            }
+        }
+
+        preCameraPos.x = aimingPosX;
+        CameraBodyTransform.localPosition = preCameraPos;
+        yield return null;
+    }
+
+    private IEnumerator AimingZoomCoroutine()
+    {
+        Transform targetTransform;
+        if (isAiming)
+        {
+            targetTransform = ZoomTransform;
+        }
+        else
+        {
+            targetTransform = NormalTransform;
+        }
+
+        var distMagnitude = Vector3.SqrMagnitude(targetTransform.localPosition - CameraTransform.localPosition);
+        while (distMagnitude > 1f)
+        {
+            CameraTransform.localPosition = Vector3.Slerp(CameraTransform.localPosition, targetTransform.localPosition, .07f);
+            distMagnitude = Vector3.SqrMagnitude(targetTransform.localPosition - CameraTransform.localPosition);
+            yield return null;
+        }
+
+        CameraTransform.localPosition = targetTransform.localPosition;
+        yield return null;
     }
 }
