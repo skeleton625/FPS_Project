3a9bc451a646eba842b711ee0b5cb0c42c100a95
Skeleton <chaos0824@naver.com>
2021-01-17 21:12:55 +0900

    달리기 기능 추가, 특정 상황 시, 속도 감소 기능 추가

--- Assets/Scripts/PlayerMovementBehaviour.cs
+++ Assets/Scripts/PlayerMovementBehaviour.cs
@@ -2,18 +2,25 @@
 
 public class PlayerMovementBehaviour : MonoBehaviour
 {
-    [SerializeField] private float MoveSpeed = 5f;
-    [SerializeField] private float TurnSpeed = .01f;
+    [SerializeField] private float DefaultMoveSpeed = 5f;
+    [SerializeField] private float DefaultSlowValue = .5f;
+    [SerializeField] private float DefaultFastValue = 1.5f;
+    [SerializeField] private float DefaultTurnSpeed = .01f;
     [SerializeField] private Transform ModelTransform = null;
     [SerializeField] private Rigidbody PlayerRigidbody = null;
 
-    private Camera mainCamera = null;
-    private Vector2 playerTurnDirection = Vector2.zero;
-    private Vector3 playerMoveDirection = Vector3.zero;
+    private bool isAimming = false;
+    private bool isRunning = false;
+    private float preMoveSpeed = 0f;
+    private float preTurnSpeed = 0f;
+    private Vector3 moveDirection = Vector3.zero;
+    private Transform cameraTransform = null;
 
     private void Awake()
     {
-        mainCamera = Camera.main;
+        preMoveSpeed = DefaultMoveSpeed;
+        preTurnSpeed = DefaultTurnSpeed;
+        cameraTransform = Camera.main.transform;
     }
 
     private void FixedUpdate()
@@ -22,40 +29,70 @@ public class PlayerMovementBehaviour : MonoBehaviour
         TurnThePlayer();
     }
 
-    public void UpdateTurnDirection(Vector2 newTurnDriection)
-    {
-        playerTurnDirection = newTurnDriection;
-    }
-
-    public void UpdateMovementDirection(Vector3 newMoveDirection)
-    {
-        playerMoveDirection = newMoveDirection;
-    }
-
     private void MoveThePlayer()
     {
-        Vector3 movement = CalculateCameraDirection(playerMoveDirection) * MoveSpeed * Time.deltaTime;
+        var moveSpeed = preMoveSpeed;
+        if (moveDirection.z < 0)
+            moveSpeed = DefaultMoveSpeed * DefaultSlowValue;
+
+        Vector3 movement = CalculateCameraDirection(moveDirection) * moveSpeed * Time.deltaTime;
         PlayerRigidbody.MovePosition(movement + transform.position);
     }
 
     private void TurnThePlayer()
     {
-        if(playerMoveDirection.sqrMagnitude > .01f)
+        if (!isAimming && moveDirection.sqrMagnitude > .01f)
         {
-            var lookRotation = Quaternion.LookRotation(CalculateCameraDirection(playerMoveDirection));
-            var rotation = Quaternion.Slerp(ModelTransform.rotation, lookRotation, TurnSpeed);
+            var lookDirection = moveDirection;
+            if (lookDirection.z < 0) lookDirection.z *= -1;
+            var lookRotation = Quaternion.LookRotation(CalculateCameraDirection(lookDirection));
+            var rotation = Quaternion.Slerp(ModelTransform.rotation, lookRotation, preTurnSpeed);
             ModelTransform.rotation = rotation;
         }
     }
 
     private Vector3 CalculateCameraDirection(Vector3 moveDirection)
     {
-        var cameraForward = mainCamera.transform.forward;
-        var cameraRight = mainCamera.transform.right;
-        
+        var cameraForward = cameraTransform.forward;
+        var cameraRight = cameraTransform.right;
         cameraForward.y = 0;
         cameraRight.y = 0;
 
         return cameraForward * moveDirection.z + cameraRight * moveDirection.x;
+    }
+
+    public void UpdateMovementDirection(Vector3 newMoveDirection)
+    {
+        moveDirection = newMoveDirection;
+    }
+
+    public void UpdateRunningState(bool isRunning)
+    {
+        if(isRunning)
+        {
+            preMoveSpeed = DefaultMoveSpeed * DefaultFastValue;
+        }
+        else
+        {
+            preMoveSpeed = DefaultMoveSpeed;
+        }
+        this.isRunning = isRunning;
+    }
+
+    public void UpdateAimmingState(bool isAimming)
+    {
+        if(isAimming)
+        {
+            preMoveSpeed = DefaultMoveSpeed * DefaultSlowValue;
+            ModelTransform.rotation = Quaternion.LookRotation(CalculateCameraDirection(Vector3.forward));
+        }
+        else
+        {
+            if (isRunning)
+                preMoveSpeed = DefaultMoveSpeed * DefaultFastValue;
+            else
+                preMoveSpeed = DefaultMoveSpeed;
+        }
+        this.isAimming = isAimming;
     }
 }
